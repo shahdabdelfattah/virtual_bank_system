302bf58f091d58dc93b79a862dceb6fbb1042d55
Shahd Abdelfatah <shahda.abdelfatah@gmail.com>
2026-07-17 16:02:10 +0300

    Initial Commit

---
+++ src/main/java/com/example/virtual_bank/VirtualBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.virtual_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/virtual_bank/VirtualBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.virtual_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
